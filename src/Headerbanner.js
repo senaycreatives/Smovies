fca30cb44261016ea068be8128ae6c7352b0698a
Andi-gech <andi.fab23@gmail.com>
2023-11-10 19:40:04 +0300

    andi

--- src/Headerbanner.js
+++ src/Headerbanner.js
@@ -1,7 +1,8 @@
 import React from "react";
-
 import { AiFillClockCircle } from "react-icons/ai";
 import { FaPlayCircle } from "react-icons/fa";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 import UseFetchMovieData from "./hooks/UseFetchMovieData";
 
@@ -10,31 +11,31 @@ export default function Headerbanner({ id }) {
 
   return (
     <div
-      className="  relative  h-full w-screen bg-zinc-700   bg-contain  flex-shrink-0"
+      className="relative h-full w-screen bg-zinc-700 bg-contain flex-shrink-0"
       style={{
         backgroundImage: `url(${data?.Poster})`,
       }}
     >
-      <div className=" bg-text  pl-3 absolute w-full h-full  flex  flex-col justify-end pb-12    ">
-        <h1 className=" font-bold  text-4xl text-white mb-5">{data?.Title}</h1>
-        <p className=" text-white w-[600px] line-clamp-2  ">{data?.Plot}</p>
+      <div className="bg-text pl-3 absolute w-full h-full flex flex-col justify-end pb-12">
+        <h1 className="font-bold text-4xl text-white mb-5">{data?.Title}</h1>
+        <p className="text-white w-[600px] line-clamp-2">{data?.Plot}</p>
         <div className="h-[50px] flex flex-row">
-          <div className=" w-[80px] font-bold text-black h-[20px] mt-3 flex items-center justify-center  rounded-full  text-xs bg-yellow-600">
+          <div className="w-[80px] font-bold text-black h-[20px] mt-3 flex items-center justify-center rounded-full text-xs bg-yellow-600">
             IMDB-{data?.imdbRating}
           </div>
-          <div className=" mx-2 w-[40px] font-bold text-black h-[20px] mt-3 flex items-center justify-center  rounded-full  text-xs bg-lime-600">
+          <div className="mx-2 w-[40px] font-bold text-black h-[20px] mt-3 flex items-center justify-center rounded-full text-xs bg-lime-600">
             HD
           </div>
-          <div className=" mx-2 flex flex-row  text-white h-[20px] mt-3  items-center justify-center  rounded-full  text-xs ">
+          <div className="mx-2 flex flex-row text-white h-[20px] mt-3 items-center justify-center rounded-full text-xs">
             <AiFillClockCircle /> <p>{data?.Runtime}</p>
           </div>
-          <div className=" mx-2 flex flex-row  text-white h-[20px] mt-3  items-center justify-center  rounded-full  text-xs ">
+          <div className="mx-2 flex flex-row text-white h-[20px] mt-3 items-center justify-center rounded-full text-xs">
             {data?.Genre}
           </div>
         </div>
-        <div className=" w-[140px]  mt-5 h-[50px] rounded-md flex items-center justify-center bg-lime-400">
-          <p className=" font-bold mx-2">Play Now</p>
-          <FaPlayCircle className=" after:text-transparent h-[40px] w-[40px]" />
+        <div className="w-[140px] mt-5 h-[50px] rounded-md flex items-center justify-center bg-lime-400">
+          <p className="font-bold mx-2">Play Now</p>
+          <FaPlayCircle className="after:text-transparent h-[40px] w-[40px]" />
         </div>
       </div>
     </div>
--- src/MovieCards.js
+++ src/MovieCards.js
@@ -1,27 +1,27 @@
 import React from "react";
-
 import { FaPlayCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 import UseFetchMovieData from "./hooks/UseFetchMovieData";
+
 export default function MovieCards({ name, id }) {
   const { data } = UseFetchMovieData(id);
 
   return (
     <Link
       to={`/movie/${id}`}
-      className=" w-[220px] mt-9  group bg-white  dark:bg-transparent  rounded-md hover:scale-105 transition-all duration-200 overflow-hidden flex-shrink-0   mx-7  h-[300px] "
+      className="w-[220px] mt-9 group bg-white dark:bg-transparent rounded-md hover:scale-105 transition-all duration-200 overflow-hidden flex-shrink-0 mx-7 h-[300px] relative"
     >
       {data?.Poster !== "N/A" ? (
-        <div
-          className=" h-[250px]  relative rounded-[5px] overflow-hidden w-full bg-cover bg-center bg-no-repeat "
-          style={{
-            backgroundImage: `url(${data?.Poster})`,
-          }}
-        >
-          <div className=" moviecard  absolute flex items-center justify-center h-full w-full group-hover:visible group-hover:opacity-60 opacity-0 transition-opacity  duration-300 cursor-pointer  invisible">
-            <FaPlayCircle size={60} className=" text-lime-950" />
-          </div>
-        </div>
+        <LazyLoadImage
+          alt={data?.Title}
+          effect="blur"
+          src={data?.Poster !== "N/A" ? data?.Poster : ""}
+          width="100%"
+          placeholderSrc="placeholder.jpg" // Provide a placeholder image
+          className="rounded-[5px] bg-cover h-[250px] bg-center bg-no-repeat"
+        />
       ) : (
         <div class="flex  justify-center w-full h-[250px] bg-gray-300 rounded   dark:bg-gray-700">
           <svg
@@ -36,25 +36,28 @@ export default function MovieCards({ name, id }) {
         </div>
       )}
 
-      <div className=" h-[50px] w-full  flex flex-col">
+      <div className="moviecard absolute w-full h-full  flex items-center justify-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity duration-300 cursor-pointer group-hover:opacity-60">
+        <FaPlayCircle size={60} className="text-lime-950" />
+      </div>
+
+      <div className="h-[50px] w-full flex flex-col">
         <div>
-          <p className=" dark:text-white   line-clamp-1 font-bold group-hover:text-lime-400">
+          <p className="dark:text-white line-clamp-1 font-bold group-hover:text-lime-400">
             {data?.Title}
           </p>
         </div>
-        <div className=" flex  flex-row justify-between">
-          <div className=" flex flex-row items-center">
-            <p className=" dark:text-zinc-400 text-zinc-500  text-sm mx-1 font-thin">
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row items-center">
+            <p className="dark:text-zinc-400 text-zinc-500 text-sm mx-1 font-thin">
               {data?.Year}
             </p>
-            <p className=" dark:text-white text-sm">.</p>
-            <p className=" dark:text-zinc-400 text-zinc-500 text-sm mx-1  font-thin">
+            <p className="dark:text-white text-sm">.</p>
+            <p className="dark:text-zinc-400 text-zinc-500 text-sm mx-1 font-thin">
               {data?.Runtime}
             </p>
           </div>
-          <div className=" h-[20px]  border-[1px]  border-opacity-25 group-hover:border-lime-300 items-center justify-center flex">
-            <p className="  dark:text-zinc-400 text-zinc-500 group-hover:text-lime-400 text-sm px-[2px] font-bold">
-              {" "}
+          <div className="h-[20px] border-[1px] border-opacity-25 group-hover:border-lime-300 items-center justify-center flex">
+            <p className="dark:text-zinc-400 text-zinc-500 group-hover:text-lime-400 text-sm px-[2px] font-bold">
               {data?.Type}
             </p>
           </div>
