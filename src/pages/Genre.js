abc61879003314fe7fe7863cb50dc32a8a11f3e8
Haileamlak Derbie <amlakhaile@gmail.com>
2023-11-14 08:39:37 -0800

    logo change

--- src/pages/Genre.js
+++ src/pages/Genre.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function Genre() {
   return (
-    <div className=" w-full h-screen  dark:bg-zinc-900 pb-6 bg-zinc-100"></div>
+    <div className=" w-full h-screen  dark:bg-zinc-900 pb-6 bg-zinc-100">
+      
+   
+
+    </div>
   );
 }
 
